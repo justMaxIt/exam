47c953d7f1204f091af89a475433ec2e161a8409
justMaxIt <maximfedor2015@gmail.com>
2020-10-12 00:42:37 +0300

    20 img to  mainContent

--- src/elements/main/MainContent.js
+++ src/elements/main/MainContent.js
@@ -1,9 +1,40 @@
-import React from "react";
-// import MainContent from "../Elements/Main/MainContent";
-// import MainMenu from "../Elements/Main/MainMenu";
+import { map } from "jquery";
+import React, {useState, useEffect} from "react";
+
+const api = "https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&language=en-US&page="
+const posterUrl = "http://image.tmdb.org/t/p/w200" 
 
 function MainContent() {
-  return <div className="main-content">go</div>;
+const [data, setData] = useState([]);
+const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    fetch(`${api}${page}`)
+      .then((res) => res.json())
+      .then((res) => setData(res.results))
+      .catch((error) => console.error(error));
+  }, [page]);
+  
+  let posterPath = data?.map(el => posterUrl + el.poster_path)
+
+
+  console.log(data, posterPath)
+  return <div className="main-content">
+   
+    <div>
+      Latest Releases
+    </div>
+    <div>
+     
+        
+    
+      <a href="#"><ul>{posterPath.map(el => <li><img src={el} alt="img" /></li>)} </ul></a>
+
+
+    </div>
+    
+    
+    </div>;
 }
 
 export default MainContent;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MainContent from "../elements/main/MainContent";
+import Menu from "../elements/menu/Menu";
 // import MainContent from "../Elements/Main/MainContent";
 // import MainMenu from "../Elements/Main/MainMenu";
 
 function MainPage() {
   return <div className="main-page">
-    <MainContent />
+        <MainContent />
   </div>;
 }
 
