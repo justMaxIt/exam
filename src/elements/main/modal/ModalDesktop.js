10ffad3fa3d6aa524c25d36151e2df93ffc5db51
justMaxIt <maximfedor2015@gmail.com>
2020-10-20 18:04:02 +0300

    style desktop Modal

--- src/elements/main/modal/ModalDesktop.js
+++ src/elements/main/modal/ModalDesktop.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Menu from "../../menu/Menu";
 import style from "./Modal.module.css"
 
@@ -8,29 +8,37 @@ const { setIsVisable, setFilmData,setFilmIndex, data, filmData, FilmIndex } = pr
 const posterUrl = "http://image.tmdb.org/t/p/w200"
 const backgroundImage = posterUrl + filmData.poster_path
   return (<div className={style.desktopPage}>
-
     <Menu />
-    <div className={style.backgroundImage} style={{ backgroundImage: `url(${backgroundImage})` }}>
-   </div>
-      
-      <div className={style.desktopContent}           >
-         <button onClick={() => setIsVisable(false)}>Back to list</button>
+    <div className={style.backgroundImage} style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+    <div className={style.desktopContent}>
+     
+      <div className={style.topButtons}>
+      <button onClick={() => setIsVisable(false)}>Back to list</button>
       <button onClick={() => {
         if (FilmIndex <= 18) {
           setFilmIndex(FilmIndex + 1);
           setFilmData(data[FilmIndex + 1])
         }
       }}>Next Movie</button>
+      </div>
 
-      <img src={posterUrl + filmData.poster_path} alt="movie poster" />
+
+      <div className={style.insideContent}>
+      <div className={style.imgModal}>
+        <img src={posterUrl + filmData.poster_path} alt="movie poster" />
+        </div>
+        <div className={style.insideFilmContent}>
+       <div className={style.buttonAddToFavorite}>
       <button>Add to favorite</button>
-
-      <div>{filmData.title}</div>
-      <div>{filmData.vote_average}</div>
-      <div>{ (filmData.adult === true)? "R" : "Pg"}</div>
-      <div>{filmData.release_date}</div>
-<div>{filmData.overview}</div>
-
+      </div>
+      
+        <div className={style.titleFilm}>{filmData.title}</div>
+        <div className={style.scoreRatingRelease}>
+      <div>Score: {filmData.vote_average}</div>
+      <div style={{borderLeft: "2px solid rgba(255, 255, 255, 0.3)", borderRight: "2px solid rgba(255, 255, 255, 0.3)" }}>Rating: { (filmData.adult === true)? "R" : "Pg"}</div>
+      <div>Release Date: {filmData.release_date}</div></div>
+<div style={{borderTop: "2px solid rgba(255, 255, 255, 0.3)", borderBottom: "2px solid rgba(255, 255, 255, 0.3)", margin: "5%", marginRight: "0", padding: "3% 0"}}>{filmData.overview}</div>
+</div></div>
     </div>
     
     </div>
--- src/elements/main/modal/ModalMobile.js
+++ src/elements/main/modal/ModalMobile.js
@@ -1,14 +1,37 @@
-import React, { useState } from "react";
-
-import style from "./../../main/modal/Modal.module.css"
+import React from "react";
+import Menu from "../../menu/Menu";
+import style from "./Modal.module.css"
 
 
 function ModalMobile(props) {
- const { setIsVisable, setFilmData, setFilmIndex, data, filmData, FilmIndex } = props;
-  return (
-    <div>
-      Mobile content
+ const { setIsVisable, setFilmData,setFilmIndex, data, filmData, FilmIndex } = props;
+const posterUrl = "http://image.tmdb.org/t/p/w200"
+const backgroundImage = posterUrl + filmData.poster_path
+  return ( <div className={style.mobilePage}>
+    
+    <div className={style.backgroundImage} style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+    <div className={style.mobileContent}           >
+         <button onClick={() => setIsVisable(false)}>Back to list</button>
+      <button onClick={() => {
+        if (FilmIndex <= 18) {
+          setFilmIndex(FilmIndex + 1);
+          setFilmData(data[FilmIndex + 1])
+        }
+      }}>Next Movie</button>
+
+      <img src={posterUrl + filmData.poster_path} alt="movie poster" />
+      <button>Add to favorite</button>
+
+      <div>{filmData.title}</div>
+      <div>Score: {filmData.vote_average}</div>
+      <div>Rating: { (filmData.adult === true)? "R" : "Pg"}</div>
+      <div>Release Date: {filmData.release_date}</div>
+<div>{filmData.overview}</div>
+
     </div>
+    
+    </div>
+
     )
 }
   
